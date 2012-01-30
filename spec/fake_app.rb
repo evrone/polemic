a869e8b404d324bcbe2e3288f140d86630c5690c
Kir <shatrov@me.com>
2012-01-30 13:31:59 +0200

    Removing old stuff

--- spec/fake_app.rb
+++ spec/fake_app.rb
@@ -88,13 +88,6 @@ end
 
 # controllers
 class ApplicationController < ActionController::Base
-  before_filter :assign_unread_discussions
-  
-  private
-  
-  def assign_unread_discussions
-    @unread_discussions_count = Discussion.unread_for(current_user).count if user_signed_in?
-  end
 end
 
 # helpers
